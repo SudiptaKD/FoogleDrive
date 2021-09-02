0fdd50cabd6df1856607f7decb066d1271167ba1
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2021-09-02 19:56:09 +0600

    Auth created

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import SignUp from "./SignUp";
+import Dashboard from "./Dashboard";
+import LogIn from "./LogIn"
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
+
+
+function App() {
+  return  (
+    <Container className="d-flex align-items-center justify-content-center " 
+    style={{ minHeight: "100vh"}}
+    >
+      <div className="w-100" style={{maxWidth:"400px"}}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute  path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component ={SignUp} />
+              <Route path="/login" component ={LogIn} />
+              <Route path="/forgot-password" component ={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App;
