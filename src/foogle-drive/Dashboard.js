47c9942a5c642f86fd7115f847a5ca443ed31487
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2021-09-03 20:24:05 +0600

    AddFolderButton & Font Awesome

--- src/foogle-drive/Dashboard.js
+++ src/foogle-drive/Dashboard.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import AddFolderButton from './AddFolderButton'
 import Navbar from "./Navbar"
 
 export default function Dashboard() {
+
     return (
         <>
             <Navbar/>
             <Container fluid>
-                Content
+                <AddFolderButton/>
             </Container>     
         </>
     )
--- src/foogle-drive/Navbar.js
+++ src/foogle-drive/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default function NavbarComponent() {
     return (
-        <Navbar bg = "secondary" expand = "sm" >
+        <Navbar bg = "secondary" variant="dark" expand = "sm" >
             <Container>
             <Navbar.Brand as={Link} to="/" >
                 Foogle Drive
